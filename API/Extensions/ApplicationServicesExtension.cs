0b1028fbc1e0a8741535b478b54850c2c3062b96
haimbensimon <81986047+haimbensimon@users.noreply.github.com>
2022-03-08 08:18:10 +0200

    end of section 13

--- API/Extensions/ApplicationServicesExtension.cs
+++ API/Extensions/ApplicationServicesExtension.cs
@@ -15,6 +15,7 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            services.AddScoped<IBasketRepository , BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.Configure<ApiBehaviorOptions>(options => {
